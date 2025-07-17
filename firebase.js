897816cca2d107f8e6410fce04595d5d7a0e5d57
Abdul khayum <130848623+Abdulkhayum2k4@users.noreply.github.com>
2025-07-17 21:56:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDT4tBbzeRln8UKwdZlTfVCoIG2LusS_s0",
-  authDomain: "wizkids-46637.firebaseapp.com",
-  projectId: "wizkids-46637",
-  storageBucket: "wizkids-46637.firebasestorage.app",
-  messagingSenderId: "402647825974",
-  appId: "1:402647825974:web:817b32f3623a4eda812297"
+  apiKey: "xxx",
+  authDomain: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx"
 };
 
 // Initialize Firebase
